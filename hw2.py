6da38be8535a860e460ad7e2dc675dad14b90058
Maxim Klykov <maxim.klykov@yandex.ru>
2020-04-22 19:38:41 +0300

    lesson 3 work

--- hw2.py
+++ hw2.py
@@ -1,4 +1,6 @@
+#
+# for i in range(1,11):
+#     number =[value *i for value in range(1,11) ]
+#     print(number)
 
-for i in range(1,11):
-    number =[value *i for value in range(1,11) ]
-    print(number)
+import random
