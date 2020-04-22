2135037cd03718ce3fee97cb5e65be0c0020a04e
Maxim Klykov <maxim.klykov@yandex.ru>
2020-04-22 19:15:40 +0300

    lesson 3

---
+++ hw2.py
@@ -0,0 +1,9 @@
+n = 'asdfafd'
+print(n)
+for i in range(1,11):
+    number = []
+    for j = in range (10):
+        number.append(i)
+    for k in range(1, 11)
+        number[k-1] *= k
+    print(number)
